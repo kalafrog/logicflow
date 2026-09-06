10e8c948cee8465743f1518af0617f4dde4bcd0c
BLACKEDOUTFROG <blackedoutfrog@gmail.com>
2026-09-06 22:05:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ RECOMMENDATIONS RULES:
                     content: prompt
                 }
             ],
-            model: "llama-3.3-70b-versatile",
+            model: "llama-3.1-70b-versatile",
             temperature: 0.1,
             response_format: { type: "json_object" }
         });
